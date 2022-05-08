caa9fb339952433b7b9390f77e8074310f25ccde
sho watanabe <showatanabe0226@gmail.com>
2022-05-08 10:33:13 +0900

    [add]DBのデータをbladeファイルに表示

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,4 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/', [PostController::class, 'index']);
