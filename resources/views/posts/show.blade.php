0ba63225d394854ed75f1be779b90e151c46491c
sho watanabe <showatanabe0226@gmail.com>
2022-05-08 21:01:58 +0900

    [add]編集ページと編集機能を追加

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -17,6 +17,7 @@
                 <p>{{ $post->body }}</p>    
             </div>
         </div>
+        <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
         <div class="footer">
             <a href="/">戻る</a>
         </div>
